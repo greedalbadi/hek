013f131f089b1c122e1bc8df76e7514a0724bfd2
greed <greedalbadi@gmail.com>
2022-01-03 20:32:49 +0400

    filenotfound exception

--- hek/exceptions.py
+++ hek/exceptions.py
@@ -5,3 +5,7 @@ class LengthError(ValueError):
     """ length error exception """
 class ProxyFailed(ConnectionRefusedError):
     """ Proxy failed to connect """
+class FileNotFound(FileNotFoundError):
+    """ if file not found """
+class ExifExtractError(InterruptedError):
+    """ exif error """
