348fba7abb8bcb3790d90072f0d2ff1c828942b5
greed <greedalbadi@gmail.com>
2022-01-11 14:46:32 +0400

    version 0.1.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='hek',
-  version='0.1.2',
+  version='0.1.3',
   description='A python library mostly used for pentesting and automation some tasks.',
   long_description=open('README.txt').read(),
   url='https://github.com/greedalbadi/hek',
