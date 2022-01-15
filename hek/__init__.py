f2223ae7ecae2278e221a7be6d0e1dd47d05b752
greed <greedalbadi@gmail.com>
2022-01-15 13:42:36 +0400

    imported system

--- hek/__init__.py
+++ hek/__init__.py
@@ -2,4 +2,5 @@ from .ipstuff import ipstuff
 from .proxy import proxy
 from .image import Image
 from .wireless import wifi
-from .network import net
+from .network import net, wireshark, arp
+from .devicemanger import system
