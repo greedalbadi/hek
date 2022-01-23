c8724f096c15fdcb7168771919ef01863e520e98
greed <greedalbadi@gmail.com>
2022-01-23 18:38:49 +0400

    0.1.8

--- setup.py
+++ setup.py
@@ -3,14 +3,16 @@ from setuptools import setup, find_packages
 
 setup(
   name='hek',
-  version='0.1.7',
+  version='0.1.8',
   description='A python library mostly used for pentesting and automation some tasks.',
   long_description=open('README.txt').read(),
   url='https://github.com/greedalbadi/hek',
   author='greed albadi',
   author_email='greedalbadi@gmail.com',
   license='MIT',
-  keywords='none',
+  keywords=["python", "pentesting", "automation", "stream", "http"],
   packages=find_packages(),
-  install_requires=['pillow', "psutil", "requests", "requests[socks]"]
+  install_requires=['pillow', "psutil", "requests", "requests[socks]",
+                    "numpy", "pyautogui", "datetime", "opencv-python"
+                    ]
 )
