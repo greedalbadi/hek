60b72cbaf83c6f07682c23a1074deb34f8600df8
greed <greedalbadi@gmail.com>
2022-01-24 11:55:19 +0400

    0.2.1 - fixed readme for pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
   name='hek',
-  version='0.1.8',
-  description='A python library mostly used for pentesting and automation some tasks.',
-  long_description=open('README.txt').read(),
+  version='0.2.1',
+  description='A python library mostly used for pentesting and automate some tasks.',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
   url='https://github.com/greedalbadi/hek',
   author='greed albadi',
   author_email='greedalbadi@gmail.com',
